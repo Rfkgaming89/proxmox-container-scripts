35804ddc1ab544a01a9e7830f74f82c6523a4335
Kai <70678154+Rfkgaming89@users.noreply.github.com>
2023-05-08 14:30:16 -0400

    Update create-container.sh
    Fixed up the code a bit to run and allows for said storage space

--- create-container.sh
+++ create-container.sh
@@ -1,45 +1,38 @@
 #!/bin/bash
 
 # Define variables
-password="your_password"
-template="debian-11-standard_11.6-1_amd64.tar.zst"
-storage="local-lvm"
-disk_size="10G"
-cpu_count="1"
-ram_size="512"
-network="vmbr0"
-ipv4_subnet="192.168.1.1/24"
-ipv6_subnet="2a02:1234:5678:9abc::/64"
-dns_servers="8.8.8.8 8.8.4.4"
+TEMPLATE_LOCATION=/mnt/pve/Backup/template/cache
+TEMPLATE_FILE=debian-11-standard_11.6-1_amd64.tar.zst
+PASSWORD="your_password_here"
+CORES=1
+MEMORY=512
+DISK_SPACE=10G
+STORAGE_DISK=Storage
 
-# Find the next available IP address based on the previous one
-last_ip="$(cat /etc/pve/nodes/*/lxc/*.conf | grep "ip_address" | awk '{print $NF}' | sort -rn | head -n1)"
-if [ -z "$last_ip" ]; then
-    next_ip="$(echo $ipv4_subnet | cut -d'.' -f1-3).2"
-else
-    next_ip="$(echo $last_ip | awk -F. '{print $1,$2,$3,$4+1}' OFS=.)"
-fi
+# Determine last container number
+LAST_CT_NUM=$(pct list | awk 'NR>1 {print $1}' | sort -n | tail -1)
+
+# Increment last container number to get new container number
+NEW_CT_NUM=$((LAST_CT_NUM + 1))
 
 # Ask for hostname
-read -p "Enter hostname: " hostname
+read -p "Enter hostname for new container: " HOSTNAME
 
-# Create the container
-pct create $(expr $(pct list | grep CT | wc -l) + 100) -hostname $hostname -password $password -net0 name=eth0,bridge=$network,ip=$ipv4_subnet,ip6=$ipv6_subnet,gw=192.168.1.1,ip6-gw=2a02:1234:5678:9abc::1 -ostemplate $template -storage $storage -rootfs $disk_size -memory $ram_size -cpus $cpu_count
+# Create new container
+pct create $NEW_CT_NUM $TEMPLATE_LOCATION/$TEMPLATE_FILE --hostname $HOSTNAME --password $PASSWORD --cores $CORES --memory $MEMORY --storage $STORAGE_DISK --rootfs $STORAGE_DISK:10 --onboot 1
 
 # Enable keyctl
-pct set $(expr $(pct list | grep CT | wc -l) + 100) -onboot 1 -startup keyctl
+pct set $NEW_CT_NUM --keyctl yes
 
-# Start the container
-pct start $(expr $(pct list | grep CT | wc -l) + 100)
+# Start container
+pct start $NEW_CT_NUM
 
-# Ask for login password
-read -p "Enter login password: " login_password
+# Run updates and install curl
+pct exec $NEW_CT_NUM -- apt update && apt upgrade -y && apt install curl -y
 
-# Log in to the container and run commands
-sshpass -p "$login_password" ssh root@$next_ip <<EOF
-apt update
-apt upgrade -y
-apt install curl -y
-sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
-service ssh restart
-EOF
+# Enable root login via SSH
+pct exec $NEW_CT_NUM -- sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+pct exec $NEW_CT_NUM -- service ssh restart
+
+# Print completion message
+echo "Container $NEW_CT_NUM created and updates applied."
